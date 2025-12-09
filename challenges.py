91020597352323939fda17d15c96d7fee2591778
m4fc <mateovasquezus@gmail.com>
2025-12-09 17:11:12 +0000

    sghaksgsj I CANTB SOLVE IT HELPPPPPEPSPEUGVYHUUBHVUSBHJNBJ BK

---
+++ challenges.py
@@ -0,0 +1,22 @@
+N=int(input("how many hours did denise work gng"))
+if N+1==24:
+    print(1)
+elif N+1>24:
+ while N+1 > 24:
+    p=N+1-1
+    print(p)
+
+
+elif N+1<24:
+   while N+1 < 24:
+     p=N+1+1
+     print(p)
+
+
+
+      
+
+
+
+
+
